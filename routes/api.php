d3962a5b0a7048cc5c1cd89de3479a21283c8be2
air <asilkhan-al@mail.ru>
2019-01-23 22:49:44 +0600

    API side is done

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,7 @@ Route::group(['middleware' => 'auth:api'], function(){
 
     Route::post('posts/{id}/delete', 'Api\PostsController@delete');
     Route::post('posts', 'Api\PostsController@store');
+    Route::post('posts/update/{id}','PostsController@update');
 
     Route::post('tags/{id}/delete', 'Api\TagsController@delete');
     Route::post('tags', 'Api\TagsController@store');
